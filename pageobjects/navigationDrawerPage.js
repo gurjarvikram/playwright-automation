171e97551cb4cb0d68d59428e1b5a42c0eee6686
gurjarvikram <vikki2k6@gmail.com>
2024-09-08 12:37:41 +0530

    Added the tests in navigation section

--- pageobjects/navigationDrawerPage.js
+++ pageobjects/navigationDrawerPage.js
@@ -1,22 +1,31 @@
-export default class navigationDrawer {
+import { time } from "console";
+
+export default class navigationBar {
 
     constructor(page) {
         this.page = page;
         this.menuIcon = '#react-burger-menu-btn';
-        this.navigationDrawerItemMenu = '.bm-item.menu-item';
-        this.cross_Drawer_Btn = '#react-burger-cross-btn';
+        this.drawerItemMenu = '.bm-item.menu-item';
+        this.aboutMenu = '#about_sidebar_link';
+        this.Cross_Btn = '#react-burger-cross-btn';
+        this.logOutLocator = '#logout_sidebar_link'
 
     }
 
-    async navigationDrawerDataPoint() {
+    async navigationDrawerMenu() {
+        const menuIconLocator = this.page.locator(this.menuIcon);
+        await menuIconLocator.waitFor({ state: 'visible' })
+        await menuIconLocator.click();
 
     }
 
     async logOut() {
+        await this.page.locator(this.logOutLocator).click()
 
     }
 
     async closingDrawer() {
+        await this.page.locator(this.Cross_Btn).click()
 
     }
 
